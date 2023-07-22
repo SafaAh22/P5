7e35865cbb242f465552885ccc1bd4fb1f514137
SafaAh22 <106147188+SafaAh22@users.noreply.github.com>
2023-07-22 14:54:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,5 @@ import streamlit as st
 
 st.write('Hello world!')
 
-user_input = st.text_input("Enter your question here")
-
-if st.button('Predict'):
-  st.write('Tags')
+title = st.text_input('Movie title', 'Life of Brian')
+st.write('The current movie title is', title)
