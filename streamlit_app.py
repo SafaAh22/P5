e7795d8850e101ca306da9d3307d4496acccf649
SafaAh22 <106147188+SafaAh22@users.noreply.github.com>
2023-08-04 14:30:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,12 @@ with st.form("my_form"):
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
-       model = joblib.load('model.pkl')
-       tags = model.predict(question)
-       st.write('question', question)
+    model = joblib.load('model.pkl')
+        # Preprocess the input question if needed
+    processed_question = [question]  # Example of putting the question into a list; you may need to do more processing here depending on your model
+
+    # Predict the tags
+    predicted_tags = model.predict(processed_question)
+    st.write('question', question)
 
 st.write("Outside the form")
