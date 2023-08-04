36daeeda5aba47b9f9c7b1cbab615856b4d1b664
SafaAh22 <106147188+SafaAh22@users.noreply.github.com>
2023-08-04 14:18:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,11 @@ st.write('Projet 5')
 with st.form("my_form"):
    st.write("Enter Your Question")
    question = st.text_input('Enter Your Question')
-   slider_val = st.slider("Form slider")
-   checkbox_val = st.checkbox("Form checkbox")
+
 
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
-       st.write("slider", slider_val, "checkbox", checkbox_val, 'question', question)
+       st.write('question', question)
 
 st.write("Outside the form")
