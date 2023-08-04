107cba16e2970caf0a12cb87008850bbcfb00150
SafaAh22 <106147188+SafaAh22@users.noreply.github.com>
2023-08-04 14:25:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,8 @@ with st.form("my_form"):
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
-       
+       model = joblib.load('model.pkl')
+       tags = model.predict(question)
        st.write('question', question)
 
 st.write("Outside the form")
