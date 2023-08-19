e1a80ddddf9544cf9bd433f024ddf915901c99fa
SafaAh22 <106147188+SafaAh22@users.noreply.github.com>
2023-08-19 10:43:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,10 @@ with st.form("my_form"):
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
-
-    # tags names
-    tag_names = ['.net','android','arrays','asp.net','c','c#','c++','css','database','django','git','html','ios','iphone','java','javascript','jquery','linux','mysql','objective-c','performance','php','python','ruby','ruby-on-rails','sql','sql-server','string','visual-studio','windows']
     
     # model
     model = joblib.load('model.pkl')
+    multilab_bin = joblib.load('multilab_bin2')
 
     # Preprocess the input question if needed
     processed_question = [question]  # Example of putting the question into a list; you may need to do more processing here depending on your model
@@ -28,8 +26,9 @@ with st.form("my_form"):
     predicted_values  = model.predict(processed_question)
 
     # Translate numerical predictions into tag names
-    # predicted_tags = [tag_names[i] for i in range(len(predicted_values[0])) if predicted_values[0][i] == 1]
+    predicted_tags = multilab_bin.inverse_transform(predicted_values)
+    st.write('predicted_tags', predicted_tags)
 
-    st.write('predicted_tags', predicted_values)
+    st.write('multilab_bin', multilab_bin.classes_)
 
 st.write("Outside the form")
